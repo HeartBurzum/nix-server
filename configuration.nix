e5a25cf36ac7066114cf6b3c7178c4c3b7fc1cfa
Patrick Todd <pattod2@gmail.com>
2025-09-26 22:38:15 -0500

    common setup for intel graphics drivers

--- configuration.nix
+++ configuration.nix
@@ -45,7 +45,21 @@
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
 
-
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-vaapi-driver # previously vaapiIntel
+      vaapiVdpau
+      libvdpau-va-gl
+      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      vpl-gpu-rt # QSV on 11th gen or newer
+      intel-media-sdk # QSV up to 11th gen
+    ];
+  };
   
 
   # Configure keymap in X11
