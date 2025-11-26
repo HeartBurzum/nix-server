dbd9acc4dbe4c6ced6a2f74a5d1f81417d1d56b8
Patrick Todd <pattod2@gmail.com>
2025-11-26 09:57:05 -0600

    dns-blocklists update

--- services/dns-blocklists/dns-blocklists.nix
+++ services/dns-blocklists/dns-blocklists.nix
@@ -1,13 +1,13 @@
 {pkgs, fetchFromGitHub, ...}:
 pkgs.stdenv.mkDerivation rec {
   pname = "dns-blocklists";
-  version = "37512025.327.52972";
+  version = "37512025.330.52951";
 
   src = fetchFromGitHub {
     owner = "hagezi";
     repo = "dns-blocklists";
     rev = "${version}";
-    sha256 = "sha256-W6POXrNvHjr3SaT1T3eSuG+iuwSfDsg7dFSUOzIg2g0=";
+    sha256 = "sha256-O0rx2zRAlqTvpectwLnUQ+DDsnNiZUXchZwkZUXm5pw=";
   };
 
   installPhase = ''
