9a778268a0d1f47e08cc34c64a3f5b89bd51199b
Patrick Todd <pattod2@gmail.com>
2026-04-08 21:16:27 -0500

    opencloud: update to 4.0.5

--- services/opencloud/opencloud-package.nix
+++ services/opencloud/opencloud-package.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 pkgs.opencloud.overrideAttrs (old: {
-  version = "4.0.0";
+  version = "4.0.5";
   src = pkgs.fetchFromGitHub {
     owner = "opencloud-eu";
     repo = "opencloud";
     tag = "v${old.version}";
-    hash = "sha256-7yl6jOJZYJjSc48ko973NWTRPjlaVkdM2u8TZ2NfR74=";
+    hash = "sha256-EseQbzQ/YRdv4b7cMl+563aQN5IcRMIZZtVqf9C4XCw=";
   };
   ldflags = [
     "-s"
@@ -13,7 +13,7 @@ pkgs.opencloud.overrideAttrs (old: {
     "-X"
     "github.com/opencloud-eu/opencloud/pkg/version.String=nixos"
     "-X"
-    "github.com/opencloud-eu/opencloud/pkg/version.Tag=4.0.0"
+    "github.com/opencloud-eu/opencloud/pkg/version.Tag=4.0.5"
     "-X"
     "github.com/opencloud-eu/opencloud/pkg/version.Date=19700101"
   ];
